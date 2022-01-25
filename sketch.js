3fa1ddeb3ba467bb55299be2dc08ee7e7306f9de
emilygc51 <73391555+emilygc51@users.noreply.github.com>
2022-01-25 14:38:56 -0600

    Finished Project

--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
 function setup() {
-  createCanvas(800, 500);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(200, 100, 150);
-  fill(200, 75, 150);
-  strokeWeight(10);
-  stroke(100, 100, 100)
-  circle(400, 250, 80);
+  x = 0;
+  y = windowHeight/2 - 100;
+  background(300);
+  strokeWeight(2);
+  stroke('#fae');
+  while(x < windowWidth) {
+    line(x, 0, x, windowWidth);
+    x = x + 4;
+  }
+  line(0, 0, windowWidth, windowHeight);
+  fill(300);
+  strokeWeight(4);
+  circle(windowWidth/2, windowHeight/2, 200);
+  strokeWeight(2);
+  while(y <= windowHeight/2 + 100) {
+    line(windowWidth/2 - 100, y, windowWidth/2 + 100, y);
+    y = y + 4;
+  }
 }
